b8b358cedafac77f1dcef1b0a819d6a632c7737a
Chandra Shekhar <shekharindian099@gmail.com>
2016-02-14 19:29:58 +0530

    basic linked list operations

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,5 +1,6 @@
 class Node
-  attr_accessor :data, :next
+  attr_reader :data
+	attr_accessor :next
   def initialize d
     @data = d
     @next = nil
@@ -11,22 +12,30 @@ class List
     @head = nil
   end
 
-  def create
-    5.times do
+  def empty?
+    @head == nil
+  end
+
+#function to insert new node
+	def insert data
+		ptr = Node.new data
+		ptr.next = @head if @head != nil
+		@head = ptr
+	end
+
+#function to create new list
+	def create
+    loop do
       puts "Enter the data for new node"
-      data = gets.chomp
-      ptr = Node.new data
-      
-      if @head == nil
-        @head = ptr
-      else
-        ptr.next = @head
-        @head = ptr        
-      end
+    	insert gets.chomp
+			puts "More Nodes(Y/n)"
+			break if (gets.chomp == 'n')
     end
   end
-  
-  def display
+
+#function to display list
+	def display
+    return nil if empty?
     temp = @head 
     while temp != nil
       puts temp.data
@@ -34,16 +43,50 @@ class List
     end
   end
 
-  def reverse temp
+#function for searching a node
+  def search data
+    return nil if empty?
+    temp = @head
+    while temp!=nil
+      return temp if temp.data == data
+      temp = temp.next
+    end
+    puts "#{data} not found"    
+  end
+
+#function to delete a node
+  def delete data
+    if @head.data == data
+      deleted = @head
+      @head = @head.next
+    else
+      temp = @head
+      while(temp.next!=nil)
+        if temp.next.data == data
+          deleted = temp.next
+          temp.next = deleted.next
+        else
+          temp = temp.next    
+        end  
+      end 
+    end
+    deleted = nil
+    p @head
+  end
+
+#function to display list in reverse order
+	def reverse temp
     return if temp == nil
     reverse temp.next
     puts temp.data
   end
 
   def printReverse
+    return nil if empty?
     reverse @head
   end
 
+#function to reverse list
   def reverseL temp
     if temp.next == nil
       @head = temp
@@ -56,6 +99,7 @@ class List
   end
 
   def listReverse
+    return nil if empty?
     reverseL @head
   end
 
@@ -64,7 +108,9 @@ end
 l = List.new
 l.create
 l.display
+l.delete 4.to_s
+l.display
 puts
-# l.printReverse
+l.printReverse
 l.listReverse
 l.display
