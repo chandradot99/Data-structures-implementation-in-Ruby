50eb599a7df316e7e486c8b5ac963095ca39cdee
Chandra Shekhar <shekharindian099@gmail.com>
2016-02-14 19:30:15 +0530

    stack operations

--- stack.rb
+++ stack.rb
@@ -2,52 +2,59 @@ class Stack
 
 	def initialize size
 		@size = size
-		@top = -1
+		@Top = -1
 		@stack = Array.new @size
 	end
 
-	def isFull
-		return (@top == @size-1) ? true : false
+	def is_full?
+		@Top == @size-1
 	end
 
-	def isEmpty
-		return (@top == -1) ? true : false
+	def is_empty?
+		@Top == -1
 	end
 
 	def push data
-		if isFull
-			puts "Stack is full"
+		if is_full?
+			puts "Stack is Overflowed"
 		else
-			@top += 1
-			@stack[@top] = data
+			@Top += 1
+			@stack[@Top] = data
 			puts "#{data} is pushed into the stack"
 		end
 	end
 
 	def pop
-		if isEmpty
-			puts "Stack is empty"
+		if is_empty?
+			puts "Stack is Underflowed"
 		else
-			puts "#{@stack[@top]} is popped out from the stack"
-			@top -= 1
+			puts "#{@stack[@Top]} is popped out from the stack"
+			@Top -= 1
 		end
+	end
+
+	def top
+		 is_empty? ? "Stack is Underflowed" : @stack[@Top]
 	end
 
 end
 
 
-puts "enter the size of the stack"
-size = gets.chomp.to_i
-s = Stack.new size
+s = Stack.new 5
 s.push 10
 s.push 20
 s.push 30
 s.push 40
 s.push 50
 s.push 60
+puts s.top
 s.pop 
+puts s.top
 s.pop
+puts s.top
 s.pop
+puts s.top
 s.pop
+puts s.top
 s.pop
-s.pop
+puts s.top
